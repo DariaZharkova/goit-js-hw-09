f6daf165183f85aa4687fc96be3181e594da676f
DariaZharkova <139045140+DariaZharkova@users.noreply.github.com>
2023-12-06 20:42:14 +0200

    add task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+
+let timerId;
+
+startButton.addEventListener('click', onStartClick);
+stopButton.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopClick() {
+  clearInterval(timerId);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
